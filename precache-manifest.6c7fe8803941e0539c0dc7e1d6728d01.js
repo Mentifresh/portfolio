bd78ea9aaf580ee7c9b761f5a2a55b7d2461c770
Daniel Kilders Diaz <dankil.dev@gmail.com>
2020-06-08 23:09:18 +0200

    Updates

---
+++ precache-manifest.6c7fe8803941e0539c0dc7e1d6728d01.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97deb09cd388c1c8185e8f93fe2f2741",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dea3e913578eddf6b331",
+    "url": "/static/css/main.ab4cc8bf.chunk.css"
+  },
+  {
+    "revision": "8a9e00cf16e2a66cd74d",
+    "url": "/static/js/2.be8e98ef.chunk.js"
+  },
+  {
+    "revision": "dea3e913578eddf6b331",
+    "url": "/static/js/main.06ec8c8f.chunk.js"
+  },
+  {
+    "revision": "695ee14e68621985b676",
+    "url": "/static/js/runtime-main.e310b0ad.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "e47c3e26372c670c4a4eeac6eea0bf98",
+    "url": "/static/media/airbnbLogo.e47c3e26.jpg"
+  },
+  {
+    "revision": "d6d4086fb1cade58b3f786620e17c3ef",
+    "url": "/static/media/contactMail.d6d4086f.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  }
+]);
